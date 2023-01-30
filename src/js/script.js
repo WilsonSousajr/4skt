0bf7ef0b877a76ae3be1b0bb6ce8e23877bf526d
Wilson Sousa <willnapolitano21@gmail.com>
2023-01-30 09:21:28 -0300

    Create active menu function for all pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const links = document.querySelectorAll(".header-menu a")
+
+function activeLink(link){
+	const url = location.href;
+	const href = link.href;
+
+	if(url.includes(href)){
+		link.classList.add('active')
+	}
+}
+
+links.forEach(activeLink)
