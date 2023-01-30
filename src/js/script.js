f1da219420c75418cc130f3b195cab3cdc6686c9
Wilson Sousa <willnapolitano21@gmail.com>
2023-01-30 10:46:27 -0300

    Create a function for skate gallery images

--- src/js/script.js
+++ src/js/script.js
@@ -45,3 +45,22 @@ function eventQuestions(question){
 }
 
 questions.forEach(eventQuestions)
+
+// Skate gallery
+
+const gallery = document.querySelectorAll(".skate-imgs img");
+const galleryContainer = document.querySelector(".skate-imgs");
+
+function changeImage(event){
+	const img = event.currentTarget
+	const media = matchMedia('(min-width: 1000px').matches  
+	if(media){
+		galleryContainer.prepend(img)
+	}
+}
+
+function eventsGallery(img){
+	img.addEventListener("click", changeImage)
+}
+
+gallery.forEach(eventsGallery)
